bcf2486f88130936ddfc4321734b6e13102d22b6
Luis Marin <luismiguelmarinovando@gmail.com>
2026-04-19 23:32:07 -0500

    cree las ventanas donde se verá el juego

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
-#from pygame import Game
-#from GameClass import Game
+from pygame import mixer
 import os
 
 #Vamos a agregar mas librerias.
 
-#Background
+pygame.init()
 
-BACKGROUND = pygame.image.load(os.path.join())
+#Game Window
+WIDTH, HEIGHT = 800, 600
+TITLE = 'Space Invaders Hybridge'
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
+
+#Background
+BACKGROUND = pygame.image.load(os.path.join('img', 'background.png'))
+ICON_IMAGE = pygame.image.load(os.path.join('img', 'title_icon.png'))
+pygame.display.set_icon(ICON_IMAGE)
+
+#player
+PLAYER_IMAGE = pygame.image.load(os.path.join('img', 'player_image.png'))
+BULLET_IMAGE = pygame.image.load(os.path.join('img', 'bullet_image.png'))
+
+try:
+    mixer.music.load(os.path.join('sounds', 'background_song.mp3'))
+except:
+    print('Music not found')
+    pass
